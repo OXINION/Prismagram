7a2d86dbb83aa6bba9f02dd02988792b6a60800a
OXINION <brandonha00918@gmail.com>
2019-07-02 20:07:15 +0900

    #3.6 Passport JWT part3

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,11 +3,14 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    requestSecret: async (_, args) => {
+    requestSecret: async (_, args, { request }) => {
+      console.log(request.user);
+
       const { email } = args;
       const loginSecret = generateSecret();
       try {
-        await sendSecretmail(email, loginSecret)
+        throw Error();
+        await sendSecretmail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
       } catch (error) {
