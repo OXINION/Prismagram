e560c587dcdb8701366c8013d81541c15fd46095
OXINION <brandonha00918@gmail.com>
2019-07-06 12:07:34 +0900

    #3.18 upload Resolver

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -9,17 +9,21 @@ export default {
       const comments = await prisma
         .post({ id })
         .comments()
-        .$fragment( COMMENT_FRAGMENT);
+        .$fragment(COMMENT_FRAGMENT);
       const likeCount = await prisma
         .likesConnection({
           where: { post: { id } }
         })
         .aggregate()
         .count();
+      const files = await prisma.post({ id }).files();
+      const user = await prisma.post({ id }).user();
       return {
         post,
         comments,
-        likeCount
+        likeCount,
+        files,
+        user
       };
     }
   }
