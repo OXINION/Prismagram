9f47ee7c1934ba29cf245a784f6d5df7ada02990
OXINION <brandonha00918@gmail.com>
2019-07-20 19:22:37 +0900

    #5.6

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,7 +3,13 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("This username is already taken");
+
+        return;
+      }
+      await prisma.createUser({
         username,
         email,
         firstName,
