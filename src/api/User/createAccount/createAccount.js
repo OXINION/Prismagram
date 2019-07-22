b6926210bae677ad0f0eaee78e862283d0ce665c
OXINION <brandonha00918@gmail.com>
2019-07-22 20:04:01 +0900

    #5.8 confirmSecret + Log in Mutation

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,11 +1,19 @@
 import { prisma } from "../../../../generated/prisma-client";
+
 export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const exists = await prisma.$exists.user({ username });
+      const exists = await prisma.$exists.user({
+        OR: [
+          {
+            username
+          },
+          { email }
+        ]
+      });
       if (exists) {
-        throw Error("This username is already taken");
+        throw Error("This username / email is already taken");
       }
       await prisma.createUser({
         username,
@@ -14,7 +22,6 @@ export default {
         lastName,
         bio
       });
-
       return true;
     }
   }
